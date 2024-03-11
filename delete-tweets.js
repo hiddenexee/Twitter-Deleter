8b9e2f3b578596046081bd038daf4f427cbcf47f
HiddenExe <161987966+hiddenexee@users.noreply.github.com>
2024-03-11 23:49:12 +0300

    Update delete-tweets.js

--- delete-tweets.js
+++ delete-tweets.js
@@ -1,1 +1,5 @@
-
+for (const r of document.querySelectorAll('[data-testid="caret"]')) {
+  document.querySelectorAll('[data-testid="caret"]')[0].click();
+  document.querySelectorAll('[role="menuitem"]')[0].click();
+  document.querySelectorAll('[data-testid="confirmationSheetConfirm"]')[0].click();
+}
